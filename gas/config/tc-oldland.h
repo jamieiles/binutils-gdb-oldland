e29ad7c7603ca2303629d3e9d323d69e13970265
Jamie Iles <jamie@jamieiles.com>
2015-03-06 01:08:34 +0000

    oldland: update opcodes.

--- gas/config/tc-oldland.h
+++ gas/config/tc-oldland.h
@@ -10,7 +10,7 @@
 #define md_estimate_size_before_relax(A, B) \
 	(as_fatal(_("estimate size\n")), 0)
 #define md_convert_frag(B, S, F) \
-	(as_fatal(_("convert_frag\n")), 0)
+	(as_fatal(_("convert_frag\n")))
 #define MD_PCREL_FROM_SECTION(FIX, SEC) \
 	md_pcrel_from(FIX)
 extern long md_pcrel_from(struct fix *f);
